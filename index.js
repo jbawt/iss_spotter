4e8e9e2622e14c83eff4c873441a8e636b22939c
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-28 01:34:42 +0000

    coordinate function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
 //     console.log("it didn't work! ", error);
 //     return;
 //   }
-//   console.log("it worked! returned IP: ", ip);
+//   console.log(ip);
 // });
+
+// fetchCoordsByIP('199.126.139.200', (error, coords) => {
+//   if (error) {
+//     console.log('nope ', error);
+//     return;
+//   }
+//   console.log(coords);
+// })
--- iss.js
+++ iss.js
@@ -3,7 +3,8 @@ const request = require('request');
 const fetchMyIP = (callback) => {
   request('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) {
-      return callback(error, null);
+      callback(error, null);
+      return;
     }
 
     if (response.statusCode !== 200) {
@@ -17,4 +18,21 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status code ${response.statusCode} when fetching coordinates. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
