1012869778c978fb94041cd3e7bc6cd00c2984a9
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-28 00:06:27 +0000

    fetched ip address complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("it didn't work! ", error);
+//     return;
+//   }
+//   console.log("it worked! returned IP: ", ip);
+// });
---
+++ iss.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ipAddress = JSON.parse(body);
+    callback(null, ipAddress);
+  });
+};
+
+module.exports = { fetchMyIP };
